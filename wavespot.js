833caf5eb5b2141186d8b03035dad1c8c97cea7b
Henning Erlandsen <henning.erlandsen@gmail.com>
2015-11-30 16:00:46 +0100

    Added scripts

---
+++ wavespot.js
@@ -0,0 +1,28 @@
+
+
+
+
+function ringMod(a, b, ammount) {
+  var makeUp = 1 / ammount;
+  ammount *= 0.5;
+  return a * (1 - (ammount * (b + 1)));
+}
+
+
+
+function square(freq, t) {
+  return (t * freq * 2) & 1;
+}
+ 
+function sine(freq, t) {
+  return Math.sin(2 * Math.PI * t * freq);   
+}
+
+function dsp(t) {
+  var base = 60;
+  var a = sine(base, t);
+  var b = sine(base * 2 + 0.1, t);
+  var c = sine(20 * base / 3 + 0.2, t);
+  var ab = ringMod(a, b, 10);
+  return 0.1 * ringMod(ab, c, 1);
+}
